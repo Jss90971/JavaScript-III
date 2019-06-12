4d74756c881e5e7084c581c9893323449010b0d2
Julian Steven Sanchez <51003438+Jss90971@users.noreply.github.com>
2019-06-12 14:54:25 -0700

    Finished
    Just need the stretch goals

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,50 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When the this value is in the global scope will now be considered the console object.
+* 2. When trying to call upon an object the this keyword is used with dot notation.
+* 3. When using a constructor function the this keyword can be used to return an object.
+* 4. When using the methods call or apply the this method is explicitly implied.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
+function testCode(code){
+  console.log(this);
+  return code;
+}
 
-// code example for Window Binding
+testCode('x + y');
 
 // Principle 2
-
-// code example for Implicit Binding
+const anObj = {
+  greet: 'Hello',
+  saysHello: function(name){
+    console.log(`${this.greet} ${this.name}`);
+  }
+};
+anObj.saysHello('Julian');
 
 // Principle 3
+function Project(cortana){
+  this.greeting = 'Good afternoon'
+  this.cortana = cortana;
+  this.speak = function(){
+    console.log(this.greeting + ' ' + this.cortana);
+  }
+};
 
-// code example for New Binding
+const siri = new Project('siri');
+
+siri.speak();
+
 
 // Principle 4
+const sports = ['Soccer', 'Football', 'Baseball'];
 
-// code example for Explicit Binding
+function sportsILike(sport1, sport2, sport3){
+  return `${sport1} is way better to watch rather than ${sport2} and ${sport3}.`
+}
+
+console.log(sportsILike(...sports));
